102c03815fd10bc5888366040cf06d75f3224a66
Alex Landherr <alex.landherr@gmail.com>
2023-10-05 17:58:35 +0000

    Solved stated problem.

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,11 +1,7 @@
 #include "include/functions.h"
 #include <cmath>
-
-/*
-A Pythagorean triplet is a set of three natural numbers, a<b<c, for which a^2 + b^2 = c^2
-For example 3^2 + 4^2 = 9 + 16 = 25 = 5^2
-There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
-*/
+#include <cstdint>
+#include <vector>
 
 namespace func {
     bool is_pythag_triplet(int a, int b, int c) {
@@ -14,5 +10,22 @@ namespace func {
         } else {
             return false;
         }
+    }
+
+    int64_t find_pythag_trip_product(int target_sum) {
+        std::vector<int64_t> all_triplet_products;
+        int64_t max_a = static_cast<int64_t>(target_sum / 3);
+
+        for (int64_t a = 1; a < max_a; a++) {
+            int64_t max_b = static_cast<int64_t>((target_sum - a) / 2);
+            for (int64_t b = a + 1; b < max_b; b++) {
+                int64_t c = target_sum - (a + b);
+                if (is_pythag_triplet(a, b, c)) {
+                    all_triplet_products.push_back(a * b * c);
+                }
+            }
+        }
+
+        return all_triplet_products.front();
     }
 }
--- src/include/functions.h
+++ src/include/functions.h
@@ -1,10 +1,11 @@
-
+#include <cstdint>
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
 namespace func {
     bool is_pythag_triplet(int a, int b, int c);
+    int64_t find_pythag_trip_product(int target_sum);
 }
 
 #endif
